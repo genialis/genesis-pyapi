2de3d3dfdde00e560a81392b09351464ae2dc80d
Miha Stajdohar <miha.stajdohar@gmail.com>
2015-04-15 11:13:44 -0500

    Added license information

--- setup.py
+++ setup.py
@@ -5,38 +5,42 @@ import os
 from setuptools import setup, find_packages
 
 
-NAME = "GenAPI"
-VERSION = "0.1.0"
-
-DESCRIPTION = "A Python API for the Genesis platform."
+NAME = 'GenAPI'
+VERSION = '0.1.1'
+DESCRIPTION = "Python API for the Genesis platform."
 LONG_DESCRIPTION = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
-AUTHOR = "Genialis d.o.o."
-AUTHOR_EMAIL = "dev-team@genialis.com"
-URL = "https://github.com/genialis/genesis-genapi/"
-LICENSE = "Proprietary software"
+AUTHOR = 'Genialis d.o.o.'
+AUTHOR_EMAIL = 'dev-team@genialis.com'
+URL = 'https://github.com/genialis/genesis-genapi/'
+LICENSE = 'BSD'
 
-if __name__ == '__main__':
-    setup(
-        name = NAME,
-        version = VERSION,
-        description = DESCRIPTION,
-        long_description = LONG_DESCRIPTION,
-        author = AUTHOR,
-        author_email = AUTHOR_EMAIL,
-        url = URL,
-        license = LICENSE,
-        packages = ['genapi', 'genapi.tests'],
-        package_data = {},
-        classifiers = [
-            'Development Status :: 4 - Beta',
-            'Environment :: Console',
-            'Intended Audience :: Developers',
-            'License :: Other/Proprietary License',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-        ],
-        include_package_data = True,
-        zip_safe = False,
-        install_requires = [r for r in open('requirements.txt').read().split("\n") if r != "" and r[:2] != "-e"],
-        test_suite = 'genapi.tests',
-    )
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    license=LICENSE,
+    packages=find_packages(),
+    package_data={},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[r for r in open('requirements.txt').read().split('\n') if r != '' and r[:2] != '-e'],
+    test_suite='genapi.tests'
+)
